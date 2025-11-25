f5f9a9b430baffaba0abb6e7bedcfadfc4d847c9
nidvar <djayshine87@hotmail.com>
2025-11-25 17:57:11 +1100

    rate limiter proxy

--- src/lib/rateLimit.ts
+++ src/lib/rateLimit.ts
@@ -1,5 +1,7 @@
 import { rateLimit } from 'express-rate-limit';
 
+
+
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 100, // Limit each IP to 10 requests per `window` (here, per 15 minutes).
--- src/server.ts
+++ src/server.ts
@@ -32,6 +32,8 @@ app.use(cors({
     credentials: true,
 }));
 
+app.set('trust proxy', 1);
+
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
