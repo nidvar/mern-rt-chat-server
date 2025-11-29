224f194ef5cddf0675340ad820e876a1a06f8458
nidvar <djayshine87@hotmail.com>
2025-11-29 14:57:17 +1100

    sockets working

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -5,6 +5,7 @@ import { Server } from 'socket.io';
 import http from 'http';
 import express from 'express';
 import { socketMiddleware } from '../middleware/socketMiddleware';
+import Message from '../models/message';
 
 const allowedOrigins = [
     'http://localhost:5173',
@@ -22,14 +23,51 @@ const io = new Server(server, {
 });
 
 io.use(socketMiddleware);
-
 const userSocketMap: Record<string, string> = {};
 
-io.on('connection', (socket)=>{
-    console.log(socket.data.user, ' - has connected to socket');
+const sendAllMessages = async function(userId: string){
+    try{
+        const allMessages = await Message.find({
+            $or: [
+                { senderId: userId},
+                { recieverId: userId}
+            ]
+        });
+
+        const socketId = userSocketMap[userId];
+        if(socketId){
+            io.to(socketId).emit('allMessagesOnLogin', allMessages)
+        };
+    }catch(err){
+        console.log(err);
+    }
+};
+
+io.on('connection', async (socket)=>{
     userSocketMap[socket.data.user.id] = socket.id.toString();
-    console.log(userSocketMap, ' - user socket map')
     io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    await sendAllMessages(socket.data.user.id);
+
+    socket.on('sendMessage', async (data: {recieverId: string, text?: string, image?: string})=>{
+        try{
+            const newMessage = await Message.create({
+                senderId: socket.data.user.id,
+                recieverId: data.recieverId,
+                text: data.text,
+                image: data.image
+            });
+
+            const recieverSocketId = userSocketMap[data.recieverId];
+            if(recieverSocketId){
+                io.to(recieverSocketId).emit('singleMessage', [newMessage])
+            }
+            io.to(socket.id).emit('singleMessage', [newMessage])
+        }catch(err){
+            console.log(err);
+        }
+    })
+
     socket.on('disconnect', ()=>{
         console.log(socket.data.user, ' - disconnected from socket');
         delete userSocketMap[socket.data.user.id];
