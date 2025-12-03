2fb808dd55e3715869d30be176f09be82d7a6d6a
nidvar <djayshine87@hotmail.com>
2025-12-04 10:48:04 +1100

    console errors

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,12 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use((req, res, next) => {
+  console.log("➡ Incoming:", req.method, req.url);
+  next();
+});
+
+
 // Cookie parser and body parsers
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
