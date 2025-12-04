77e05f0b55a655c0c360eeb6dbb483bbd0552743
nidvar <djayshine87@hotmail.com>
2025-12-04 13:12:24 +1100

    body parser moved up

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,13 @@ const server = http.createServer(app);
 app.set('trust proxy', 1);
 
 // ---------------------
+// BODY PARSERS + COOKIE
+// ---------------------
+app.use(cookieParser());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+// ---------------------
 // LOGGING MIDDLEWARE
 // ---------------------
 app.use((req, res, next) => {
@@ -48,12 +55,7 @@ app.use(cors({
   credentials: true
 }));
 
-// ---------------------
-// BODY PARSERS + COOKIE
-// ---------------------
-app.use(cookieParser());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 
 // ---------------------
 // API ROUTES
